7d14413cd0f3a9137c691e5e0252b3a13085d791
Sabbir Ahmed <sabbirahmed@Sabbirs-MacBook-Air.local>
2021-09-20 01:17:20 +0600

    create cart page and cookies for add to cart button .

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,7 @@
 import React, { useContext  } from "react";
 import Head from 'next/head';
 import NextLink from 'next/link';
-import { AppBar, Container, Link, Toolbar, Typography,  ThemeProvider, CssBaseline, Switch } from "@material-ui/core";
+import { AppBar, Container, Link, Toolbar, Typography,  ThemeProvider, CssBaseline, Switch, Badge } from "@material-ui/core";
 import useStyles from "../utils/style";
 import { createTheme } from '@material-ui/core/styles';
 import { Store } from '../utils/Store';
@@ -10,7 +10,7 @@ import Cookies from "js-cookie";
 
 const Layout = ({ title, description, children}) => {
   const { state, dispatch } = useContext(Store);
-  const {darkMode} = state;
+  const {darkMode, cart} = state;
     const theme = createTheme({
         typography: {
             h1: {
@@ -65,7 +65,15 @@ const Layout = ({ title, description, children}) => {
                     <div>
                         <Switch checked={darkMode} onChange={darkModeChangeHandler} ></Switch>
                         <NextLink href="/cart" passHref>
-                            <Link>Cart</Link>
+                            <Link>
+                            {cart.cartItems.length > 0 ? (
+                                <Badge 
+                                color= "secondary"
+                                badgeContent= {cart.cartItems.length} > Cart </Badge>
+                            ) : (
+                                'Cart'
+                            )}
+                            </Link>
                         </NextLink>
                         <NextLink href="/login" passHref>
                             <Link>Login</Link>
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,24 +1,33 @@
-import React from "react";
-import { useRouter } from "next/router";
-import data from '../../utils/data'
+import React, { useContext } from "react";
 import Layout from "../../components/Layout";
 import NextLink from 'next/link';
 import Image from 'next/image';
 import { Button, Card, Grid, Link, List, ListItem, Typography } from "@material-ui/core";
 import useStyles from "../../utils/style";
+import Product from "../../models/Product";
+import db from "../../utils/db";
+import { Store } from "../../utils/Store";
+import axios from "axios";
 
 
 
 
 
 
-const ProductScreen = () => {
+const ProductScreen = (props) => {
+    const {dispatch} = useContext(Store);
+    const {product} = props;
     const classes = useStyles();
-    const router = useRouter();
-    const { slug } = router.query;
-    const product = data.products.find((a) => a.slug === slug);
+    
     if(!product) {
         return <div> Product not found</div>;
+    }
+    const addToCartHandler = async () => {
+        const { data } = await axios.get(`/api/products/${product._id}` );
+        if (data.countInStock <= 0) {
+            window.alert('Sorry, Product is out of stock');
+        }
+        dispatch({ type: 'CART_ADD_ITEM', payload: {...product, quantity: 1 } })
     }
     return ( 
         <Layout title = {product.name} description = {product.description} >
@@ -68,7 +77,9 @@ const ProductScreen = () => {
                                 </Grid>
                             </ListItem>
                             <ListItem>
-                                <Button fullWidth variant="contained" color="primary" >
+                                <Button fullWidth variant="contained" color="primary" 
+                                    onClick={addToCartHandler}
+                                >
                                     Add to cart
                                 </Button>
                             </ListItem>
@@ -81,5 +92,19 @@ const ProductScreen = () => {
         </Layout>
      );
 }
+
+export async function getServerSideProps(context){
+    const {params} = context;
+    const {slug} = params;
+
+    await  db.connect();
+    const product = await Product.findOne({slug}).lean();
+    await db.disconnect();
+    return {
+      props: {
+        product: db.convertDocToObj(product),
+      },
+    };
+  }
  
 export default ProductScreen;
