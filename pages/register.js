391a2fa77a9b232ca768052c3099f292a3782ad5
Sabbir Ahmed <sabbirahmed@Sabbirs-MacBook-Air.local>
2021-09-21 04:25:12 +0600

    create login and regsiter page and conntect with mongodb database

--- pages/register.js
+++ pages/register.js
@@ -8,7 +8,7 @@ import { Store } from '../utils/Store';
 import { useRouter } from 'next/router';
 import Cookies from 'js-cookie';
 
-export default function Login() {
+export default function Register() {
     const router = useRouter();
     const { redirect } = router.query; // login? redirect=/shipping
     const { state, dispatch } = useContext(Store);
@@ -21,14 +21,22 @@ export default function Login() {
     
 
 
+    const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
 
     const classes = useStyles();
     const submitHandler = async (e) => {
         e.preventDefault();
+        if (password !== confirmPassword) {
+            alert("Passwords don't match");
+            return;
+
+        }
         try {
-            const { data } = await axios.post('/api/users/login', {
+            const { data } = await axios.post('/api/users/register', {
+                name, 
                 email, 
                 password,
             });
@@ -42,12 +50,22 @@ export default function Login() {
         
     }
     return (
-        <Layout title="Login">
+        <Layout title="Register">
             <form onSubmit={submitHandler} className={classes.form}>
                 <Typography component="h1" variant="h1">
-                    Login
+                    Register
                 </Typography>
                 <List>
+                    <ListItem>
+                        <TextField
+                           variant="outlined"
+                           fullWidth
+                           id="name"
+                           label="Name" 
+                           inputProps={{type: 'text'}}
+                           onChange={ (e) => setName(e.target.value)}
+                        ></TextField>
+                    </ListItem>
                     <ListItem>
                         <TextField
                            variant="outlined"
@@ -69,18 +87,29 @@ export default function Login() {
                         ></TextField>
                     </ListItem>
                     <ListItem>
+                        <TextField
+                           variant="outlined"
+                           fullWidth
+                           id="confirmPassword"
+                           label="Confirm Password" 
+                           inputProps={{type: 'password'}}
+                           onChange={ (e) => setConfirmPassword(e.target.value)}
+                        ></TextField>
+                    </ListItem>
+                    <ListItem>
                         <Button 
                             variant="contained" 
                             type="submit" 
                             fullWidth 
                             color="primary" 
                         >
-                            Login
+                            Register
                         </Button>
                     </ListItem>
                     <ListItem>
-                        Don't have an account? &nbsp;
-                        <NextLink href={`/register?redirect=${redirect || '/'}`} passHref><Link>Register</Link></NextLink>
+                        Already have an account? &nbsp;
+                        <NextLink href={`/login?redirect=${redirect || '/'}`} passHref>
+                            <Link>Login</Link></NextLink>
                     </ListItem>
                     
 
--- utils/Store.js
+++ utils/Store.js
@@ -9,7 +9,10 @@ const initialState = {
         cartItems: Cookies.get('cartItems')
         ? JSON.parse(Cookies.get('cartItems'))
         : [],
-    }
+    },
+    // userInfo: Cookies.get('userInfo')
+    //  ? JSON.parse(Cookies.get('userInfo'))
+    //  : null,
 };
 
 function reducer(state, action){
@@ -40,6 +43,11 @@ function reducer(state, action){
             Cookies.set('cartItems', JSON.stringify(cartItems))
             return { ...state, cart: {...state.cart, cartItems}};
         }
+        case 'USER_LOGIN':
+            return { ...state, userInfo: action.payload};
+        case 'USER_LOGOUT':
+            return { ...state, userInfo: null, cart: {cartItems: [] } };
+
         default:
             return state;
     }
